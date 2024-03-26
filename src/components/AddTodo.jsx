f791dca32009c6cbc6860a1f73f416d8d1c04847
pratap-abhay <abhaypratap9871@gmail.com>
2024-03-26 21:59:01 +0530

    initial commit

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,26 @@
+import styles from "./AddTodo.module.css";
+
+function AddTodo() {
+  return (
+    <div className="container ">
+      <div className="row kg-row">
+        <div className="col-6">
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Enter Todo Here"
+          />
+        </div>
+        <div className="col-4">
+          <input className={styles.input} type="date" />
+        </div>
+        <div className="col-2">
+          <button type="button" className="btn btn-success kg-button">
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default AddTodo;
